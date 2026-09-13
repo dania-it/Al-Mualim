fe7734d26367300a81e0417770efe178f26ada47
dania-it <hpleen276@gmail.com>
2026-09-13 12:52:00 +0300

    update backend url to render

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://al-mualim.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
